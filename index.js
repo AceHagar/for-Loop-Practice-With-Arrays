261e9563e32f152236d3233eb88718bfa071cfb8
SeanHaag <84480666+AceHagar@users.noreply.github.com>
2021-06-09 16:29:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// create an array variable containing the names
+let familyNames = ["Felicia", "Andy", "Mandy", "Jon", "Heather", "Holly", "Brandon", "Britany", "Josiah", "Sam", "Hannah", "Seth", "Kendra"]
+// write a for loop that prints each name on a different line
+for (let i = 0; i < familyNames.length; i ++) {
+  console.log(familyNames[i])
+}
